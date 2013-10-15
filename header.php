6af3d7dd1b49175729d3ec0c6dd358d0fe9ef383
MickaelDusaiwoir <mikanono01@hotmail.com>
2013-10-15 09:07:14 +0200

    Correction bug css et ajout de viewport

--- header.php
+++ header.php
@@ -4,6 +4,8 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php bloginfo( 'title' ) ?></title>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<link rel="stylesheet" href="<?php echo( get_bloginfo('template_directory') ); ?>/css/screen.css">
 		<script src="<?php echo( get_bloginfo('template_directory') ); ?>/js/modernizr.js"></script>
 	</head>
